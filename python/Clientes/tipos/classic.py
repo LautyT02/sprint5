326566f09b815a844b88332021289eb2a8f978a8
LautyT02 <lautyt02@gmail.com>
2022-09-06 16:41:23 -0300

    Contructor Cliente Classic
    Límite de extracción, Comisión transacción, etc

--- python/Clientes/tipos/classic.py
+++ python/Clientes/tipos/classic.py
@@ -1,1 +1,9 @@
 from ..cliente import Cliente
+
+class Classic(Cliente):
+    def __init__(self, nombre='', apellido='', numero=0, dni=11111111):
+        super().__init__(nombre, apellido, numero, dni)
+        self.habilitarTipoTarjeta('deb')
+        self.habilitarTipoCuenta('CAP')
+        self.comisionTransaccion=0.01
+        self.limiteExtraccion=10000
