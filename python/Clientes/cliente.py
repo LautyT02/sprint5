f82e7b7463aca9de2a28183dec97860157eac159
LautyT02 <lautyt02@gmail.com>
2022-09-06 11:45:01 -0300

    Modificación Cliente
    Avance en Constructor

--- python/Clientes/cliente.py
+++ python/Clientes/cliente.py
@@ -2,7 +2,17 @@
 En esta clase se establecen las caracteristicas de todo cliente.
 En tipos se hereda de esta clase y se modifican y agregan las caracteristicas necesarias.
 """
+#self.chequeras=[] Poner solo en clientes que puedan tener chequera
 class Cliente(object):
+    
+
     def __init__(self,nombre='',apellido=''):
         self.nombre=nombre
-        self.apellido=apellido
+        self.apellido=apellidoTarjeta={'deb': False, 'cred': False}
+        self.tarjetas=[]
+        self.puedeCuenta={'CAP': False, 'CAD': False, 'CC': False}
+        self.cuentas=[]
+        self.limiteExtraccion=0.0
+        self.comisionTransaccion=0.0
+
+        
