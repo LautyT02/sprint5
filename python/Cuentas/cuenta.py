27d1b2cfb3b798e1fb64ce56c24c5ca18663afdd
LautyT02 <lautyt02@gmail.com>
2022-09-06 12:37:22 -0300

    Modificación Cuenta
    Se comenzó a trabajar en el constructor y en __str__

--- python/Cuentas/cuenta.py
+++ python/Cuentas/cuenta.py
@@ -2,3 +2,12 @@
 En esta clase se establecen las caracteristicas de todo cliente.
 En tipos se hereda de esta clase y se modifican y agregan las caracteristicas necesarias.
 """ 
+class Cuenta(object):
+    def __init__(self,numero,tipo):
+        self.numero=numero
+        self.tipo=tipo
+        self.saldo=0
+    
+    def __str__(self) -> str:
+        pass
+
