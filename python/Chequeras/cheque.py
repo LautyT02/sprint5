40f6470fdcf07649917d529d3551d744b8a1a7d2
LautyT02 <lautyt02@gmail.com>
2022-09-08 02:32:34 -0300

    Simplificacion
    Eliminacion Clases Chequera y Cheque

--- python/Chequeras/cheque.py
+++
@@ -1,8 +0,0 @@
-from python.Cuentas.cuenta import Cuenta
-
-
-class Cheque(object):
-    def __init__(self,remitente=Cuenta(),destinatario=Cuenta(),monto=0.0) -> None:
-        self.remitente=remitente
-        self.destinatario=destinatario
-        self.monto=monto
--- python/Chequeras/chequera.py
+++
@@ -1,8 +0,0 @@
-from .cheque import Cheque
-
-class Chequera(object):
-    def __init__(self) -> None:
-        self.cheques=[]
-
-    def agregarCheque(self,cheque=Cheque()):
-        self.cheques.append(cheque)
