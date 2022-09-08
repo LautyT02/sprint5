c1deff21ce6f3cae44cfb2ad9ef38dd9d37013a2
LautyT02 <lautyt02@gmail.com>
2022-09-08 10:31:16 -0300

    Método str Dirección

--- python/Direccion/direccion.py
+++ python/Direccion/direccion.py
@@ -5,3 +5,13 @@ class Direccion(object):
         self.ciudad=ciudad
         self.provincia=provincia
         self.pais=pais
+
+    def __str__(self) -> str:
+        _str='"direcccion: "{ \n'
+        _str+='"calle": ' + self.calle + ', \n'
+        _str+='"numero": ' + str(self.numero) + ', \n'
+        _str+='"ciudad": ' + self.ciudad + ', \n'
+        _str+='"provincia": ' + self.provincia + ', \n'
+        _str+='"pais": ' + self.pais + '\n'
+        _str+="}"
+        return _str
